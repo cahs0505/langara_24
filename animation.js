3598a6ce93dcd8190739c1ab95fe238e40f4f82c
Nayla <naylaj10@gmail.com>
2024-09-21 14:43:35 -0700

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,13 +1,47 @@
 (() => {
     let body = document.querySelector("body");
     console.log("Running IFFE.");
-    async function getFile() { let file = await fetch("song1_map.txt"); 
+    let noteTimes;
+    async function getFile() {
+        let file = await fetch("song1_map.txt");
         let txt = await file.text();
-        console.log(txt);
+        noteTimes = txt.split(/[\r\t\n]/);
+        noteTimes = noteTimes.filter(function (e) { return e != "" });
+        console.log(noteTimes);
     };
     getFile();
-    let gameWindow = document.createElement("div");
+    let song = new Audio("Bonetrousle.mp3");
+    document.addEventListener("keyup", regKey);
+    let playing = false;
+    let currTime = 0;
+    let noteNumber = 0;
+    function regKey(e) {
+        if (e.code == "Space") {
+            if (!playing) {
+                function countUp() {
+                    currTime++;
+                    if (noteTimes.includes(currTime / 1000)) {
+                        console.log("Hit note: " + currTime + "/" + noteTimes[noteNumber]);
+                        noteNumber++;
+                    }
+                    console.log(song.currentTime);
+                }
+                song.play();
+                let timer = setInterval(countUp, 1);
+                playing = true;
+            }
+        }
+    }
+    let gameWindow = document.createElement("canvas");
     gameWindow.classList.add("game");
-    let ctx = document.querySelector(".game").getContext("2d");
+    gameWindow.style.margin = 0;
+    gameWindow.setAttribute("height", "1080");
+    gameWindow.setAttribute("width", "1920");
+    gameWindow.style.background = "blanchedalmond";
+    let ctx = gameWindow.getContext("2d");
     body.appendChild(gameWindow);
+
+    window.onload = () => {
+
+    }
 })();
