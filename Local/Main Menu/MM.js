4845455d4a22a28bdb9030b48ee3d0de7a00bb1f
Adam <63253346+Adamcs50x@users.noreply.github.com>
2024-09-21 16:18:04 -0700

    Add files via upload

---
+++ Local/Main Menu/MM.js
@@ -0,0 +1,13 @@
+function signup() {
+    window.location.href = "../signup/SU.html"
+}
+
+function login() {
+    window.location.href = "../username/UN.html";
+}
+
+function quit() {
+    window.close();
+}
+console.log("JavaScript is working!");
+
