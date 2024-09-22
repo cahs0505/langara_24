765b3311e130efbd05ec68f4a4296d38062aaf41
Nayla <naylaj10@gmail.com>
2024-09-22 13:47:05 -0700

    Add files via upload

---
+++ MMScript.js
@@ -0,0 +1,19 @@
+function signup() {
+    window.location.href = "../signup/SU.html"
+}
+
+function login() {
+    window.location.href = "../username/UN.html";
+}
+
+function game(){
+    window.location.href = "/game.html";
+}
+function guest() {
+    window.location.href = "../welcomepage/Welcome.html";
+}
+function quit() {
+    window.close();                 
+}
+console.log("JavaScript is working!");
+
